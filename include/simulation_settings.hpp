84456a5ad3b20073401ce6bd2e609b4c0bbc6337
Nabiel Kandiel <nabkan5@gmail.com>
2025-10-20 22:30:50 -0400

    add readme

--- include/simulation_settings.hpp
+++ include/simulation_settings.hpp
@@ -2,7 +2,7 @@
 #include <string>
 
 struct SimulationSettings {
-    bool load_config(const std::string& filename = "conf/settings.json");
+  bool load_config(const std::string &filename = "data/settings.ini");
 
-    int num_balls = 12; 
+  int num_balls = 12;
 };
